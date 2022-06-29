b3aaaebcab386c25dba411d5129d60dc5ea3d3b4
ParkDongho <parkdongho28@naver.com>
2022-06-29 22:11:32 +0900

    Update : auto_setup.sh

--- mac/auto_setup.sh
+++ mac/auto_setup.sh
@@ -1,6 +1,11 @@
+#설정 파일 복사
+mkdir ~/.config/nvim
+cp ./file/init.vim ~/.config/nvim
+cp ./file/coc-settings.json ~/.config/nvim
+
 #diagrammer 설치
 brew install graphviz
-cd ~/Applications
+cd ~/Library
 git clone https://github.com/freechipsproject/diagrammer
 
 export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
@@ -17,13 +22,9 @@ brew install curl
 brew install nodejs
 brew install yarn
 
-
 curl -sL install-node.vercel.app/lts | zsh
 
 cd ~/.vim/plugged/coc.nvim/
 yarn install
 yarn build
-
-
-
 
