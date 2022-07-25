6b425112fbe51bc05391fc67984b0edc0b2f3d42
ParkDongho <32865444+ParkDongho@users.noreply.github.com>
2022-07-26 02:28:17 +0900

    Update local_setup.sh

--- linux/local_setup.sh
+++ linux/local_setup.sh
@@ -1,3 +1,10 @@
+
+
+#설정 파일 복사
+mkdir ~/.config/nvim
+cp ../file/init.vim ~/.config/nvim
+cp ../file/coc-settings.json ~/.config/nvim
+
 sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
        https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
 sudo yum install curl
