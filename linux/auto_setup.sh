2c2e661fc482349a8be5b10feb3d5db9ed79b6b3
ParkDongho <parkdongho28@naver.com>
2022-07-26 00:48:52 +0900

    update linux

--- linux/auto_setup.sh
+++ linux/auto_setup.sh
@@ -1,33 +1,47 @@
 #!/bin/sh
+yum –y install vsftpd
+yum –y install epel-release
+yum –y install xrdp tigervnc-server gtk-vnc*
+
+yum -y install ftp libXt* libXt*.i686 libXext* libXext*.i686 libXScrnSaver libXScrnSaver.i686 ncurses* ncurses*.i686 sblim* ksh* libgl* libgl*.i686 mesa-* mesa-*.i686 libXp libXp.i686 libXrender libXrender.i686 glibc* glibc*.i686 fftw libreadline* libreadline*.i686 openmotif* openmotif*.i686 compat-readline* compat-readline*.i686 xorg*font* xterm redhat-lsb redhat-lsb.i686
+
 
 #설정 파일 복사
 mkdir ~/.config/nvim
-cp ./file/init.vim ~/.config/nvim
-cp ./file/coc-settings.json ~/.config/nvim
+cp ../file/init.vim ~/.config/nvim
+cp ../file/coc-settings.json ~/.config/nvim
 
 #diagrammer 설치
-brew install graphviz
-cd ~/Library
-git clone https://github.com/freechipsproject/diagrammer
-
-export JAVA_HOME=$(/usr/libexec/java_home -v 1.8)
-source ~/.bash_profile
+#brew install graphviz
+#cd ~/Library
+#git clone https://github.com/freechipsproject/diagrammer
 
 #neovim 설치
-cd
-brew install nvim
-brew install cmake
-brew install luarocks
-brew install pkg-config
 
-brew install curl
-brew install nodejs
-brew install yarn
+# prepare
+sudo dnf install curl
+# for the sake of running appimage:
+sudo dnf install fuse-libs
 
-curl -sL install-node.vercel.app/lts | zsh
+# get appimage binary
+curl -o /usr/local/bin/nvim -LO https://github.com/neovim/neovim/releases/download/stable/nvim.appimage
+# to get the nightly version instead:
+#curl -o /usr/local/bin/nvim -LO https://github.com/neovim/neovim/releases/download/nightly/nvim.appimage
 
+# make it executable
+chmod u+x /usr/local/bin/nvim
+
+sh -c 'curl -fLo "${XDG_DATA_HOME:-$HOME/.local/share}"/nvim/site/autoload/plug.vim --create-dirs \
+       https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim'
+sudo yum install curl
+
+# CoC setup
+curl -sL install-node.now.sh/lts | sudo bash
+
+sudo yum install nodejs
+sudo npm install -g yarn
 cd ~/.vim/plugged/coc.nvim/
 yarn install
 yarn build
 
-
+sudo npm install -g @imc-trading/svlangserver
